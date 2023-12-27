9a4fa62027e4ac7423a241170cd122a36c1185d2
Nic Janis <nicolas.d.janis@gmail.com>
2023-12-27 15:37:35 -0500

    update: Database search

--- 汉字数据库的事理.py
+++ 汉字数据库的事理.py
@@ -1,15 +1,45 @@
 import json
 import pandas as pd
 
-class Database():
+
+class Database:
     def __init__(self):
-        with open('汉字数据库.json' , 'r') as fp:
+        with open("汉字数据库.json", "r") as fp:
             self._data = json.loads(fp.read())
 
-        self._database_normalized = pd.json_normalize(self._data, record_path = ['汉字数据库'])
+        self._DB_KEYS = {
+            "entryCharacter": "charater",
+            "entryPin1yin1": "pin1yin1",
+            "entryEnglish": "definition",
+            "entryPOSL": "entryPOSL",
+        }
+        self._database_normalized = pd.json_normalize(self._data, record_path=["汉字数据库"])
 
     def get_database(self) -> pd.DataFrame:
+        """Get the normalized json database in a Pandas Dataframe
+
+        Returns:
+            pd.DataFrame: Normalized JSON DB
+        """
         return self._database_normalized
 
-    def search_database(self, valueDict) -> list[str]:
-        pass
+    def search_database(self, searchEntries: dict[str, str]) -> list[str]:
+        """Query Database based on user search values
+
+        Args:
+            searchEntries (dict[str, str]): A dictionary of key value pairs containing the user's search values
+
+        Returns:
+            list[str]: A collection of the queried results
+        """
+        searchValues = {
+            self._DB_KEYS.get(key): searchEntries.pop(key)
+            for key in searchEntries.copy().keys()
+        }
+        refinedDB = self._database_normalized.copy()
+
+        for filter, value in searchValues.items():
+            if filter == "entryPOSL":
+                filter = "lesson" if value[0].isdigit() else "POS"
+
+            refinedDB = refinedDB[refinedDB[filter].str.contains(value)]
